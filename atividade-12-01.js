50416e3680b6bea218820e2d3771dfd58b0e271e
PCGUIMARAES <pessoal@pcguimaraes.com>
2020-07-09 00:19:36 -0300

    lista de exercicios 12 | parcial

---
+++ atividade-12-01.js
@@ -0,0 +1,33 @@
+/* Criar um algoritmo que receba 2 carros e verifique se os carros são iguais
+Para isso :
+Crie uma classe Carro que receba as variáveis nome, modelo e cor no construtor e
+inicialize as propriedades com estes valores.
+Após isso informe ao usuário para cadastrar o primeiro carro e peça, o nome, modelo e
+cor.
+Após informar o primeiro carro peça o segundo carro também solicitando nome, modelo
+e cor.
+Ao final o algoritmo deve comparar se os carro são iguais.
+Um carro vai ser igual ao outros se modelo e nome possuem o mesmo valor.*/
+const rs = require('readline-sync')
+class Carro {
+    constructor(nome, modelo, cor){
+        this.nome = nome
+        this.modelo = modelo
+        this.cor = cor
+    }
+}
+var carros = []
+var quantidadeCarros = 2
+for(i=0;i<quantidadeCarros;i++){
+    console.log(`///// CADASTRAR ${i+1}o CARRO:`)
+    var nome = rs.question('Nome: ')
+    var modelo = rs.question('Modelo: ')
+    var cor = rs.question('Cor: ')
+    carros.push(new Carro(nome, modelo, cor))
+}
+
+if(carros[0].nome == carros[1].nome && carros[0].modelo == carros[1].modelo){
+    console.log('Carros iguais!')
+}else{
+    console.log('Carros diferentes!')
+}
---
+++ atividade-12-02.js
@@ -0,0 +1,43 @@
+/* 2. Crie um algoritmo que cadastre 3 alunos e diga qual possui a maior idade, se mais de
+um possuir maior idade apresentar o nome de todos os que possuem maior idade.
+Para isso crie uma classe Aluno que receba duas variáveis no construtor nome e idade
+e inicialize essas variáveis nas propriedades do objeto.
+Após isso peça para o usuário cadastrar o primeiro aluno e solicite nome e idade.
+Crie o objeto do aluno e adicione em um array.
+Repita esse passo para os outros alunos.
+Ao final o algoritmo deve iterar pelo array de alunos e descobrir quais são os alunos
+com maior idade e imprimir seus nomes.*/
+const rs = require('readline-sync')
+class Aluno{
+    constructor(nome, idade){
+        this.nome = nome
+        this.idade = idade
+    }
+}
+var aluno
+var alunos = []
+var maxAlunos = 3
+var maiorIdade = 18
+
+console.log('Cadastro de alunos')
+for(i=1;i<=maxAlunos;i++){
+    console.log(`aluno ${i}`)
+    nome = rs.question('Nome: ')
+    idade = rs.questionInt('Idade: ')
+    aluno = new Aluno(nome, idade)
+    alunos.push(aluno)    
+}
+
+qtdeAlunosMaiores = []
+for(i=0;i<alunos.length;i++){
+    if(alunos[i].idade>=maiorIdade){
+        qtdeAlunosMaiores.push(alunos[i].nome)
+    }
+}
+
+if(qtdeAlunosMaiores.length > 1){
+    console.log('Alunos com maioridade:')
+    for(i=0;i<qtdeAlunosMaiores.length;i++){
+        console.log(qtdeAlunosMaiores[i])
+    }
+}
